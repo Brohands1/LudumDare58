85cb11b141c2fc3b64a140bb9cb1e9aab4e8f1d7
c18357474020-ctrl <c18357474020@gmail.com>
2025-10-05 11:56:13 +0800

    a

---
+++ Assets/Scripts/ControllerSystem.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerSystem : MonoBehaviour
+{
+    [System.Serializable]
+    public class Controller
+    {
+        public Independent independent;
+        public Dependent dependent;
+    };
+
+    public List<Controller> controllers = new List<Controller>();
+    void Update()
+    {
+        foreach (var controller in controllers)
+        {
+            if (Vector3.Distance(controller.independent.transform.position, GameObject.FindGameObjectWithTag("Player").transform.position) < 3)
+            {
+                //Debug.Log("Press F to interact with the lever");
+                //可以加上文字提示，比如“按F与拉杆交互”
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    controller.independent.Active = !controller.independent.Active;
+                    controller.dependent.changeTo(controller.independent.Active);
+                }
+                if (Input.GetKeyDown(ShadowUtil.summonControllerKey)&&Data.currentShadows>1&&controller.independent.Shadowed==false&&Data.enableSummonController)
+                {
+                    Data.currentShadows--;
+                    Debug.Log($"Create controller shadow{Data.currentShadows}");
+                    controller.independent.Shadowed = true;
+                    for (int i = 0; i < Data.keys.Length; i++)
+                    {
+                        if (Data.occupied[i] == false)
+                        {
+                            Data.shadows.Add(new Data.shadow(Data.shadow.Type.controller,controller,i));
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/floatingPlatform.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floatingPlatform : Dependent
+{
+    public Transform placeIn;
+    Vector3 startPlace, endPlace;
+    public bool goingToEnd = false;
+    public float speed = 20f;
+    void Start()
+    {
+        startPlace = transform.position;
+        endPlace = placeIn.position;
+    }
+    public override void changeToTrue()
+    {
+        goingToEnd=true;
+    }
+    public override void changeToFalse()
+    {
+        goingToEnd=false;
+    }
+    void Update()
+    {
+        if (goingToEnd)
+        {
+            transform.position= Vector3.MoveTowards(transform.position, endPlace,speed* Time.deltaTime);
+        }else transform.position= Vector3.MoveTowards(transform.position, startPlace,speed* Time.deltaTime);
+    }
+}
